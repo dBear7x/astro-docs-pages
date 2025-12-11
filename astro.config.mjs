2ee51e1151a8c24819fa0ca3f335f1695d93176d
dBear7x <30729729+dBear7x@users.noreply.github.com>
2025-12-11 18:00:05 +0800

    theme: update to next.js style (#9)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import starlight from '@astrojs/starlight';
 import { defineConfig } from 'astro/config';
+import starlightThemeNext from 'starlight-theme-next';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
 			favicon: '/favicon.png',
+			plugins: [starlightThemeNext()],
 			title: 'iBear Docs',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/dBear7x/astro-docs-pages' }],
 			sidebar: [
